05feb9f1b3ab2a5d5a087a1ab3a4bfe39ad62d95
Asal Razavi <asal.razavi1381@gmail.com>
2024-03-30 16:14:47 +0330

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import Home from "./pages/Home";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />{" "}
+        <Route path="/about" element={<About />} />{" "}
+        <Route path="/sign-in" element={<SignIn />} />{" "}
+        <Route path="/sign-up" element={<SignUp />} />{" "}
+        <Route path="/dashboard" element={<Dashboard />} />{" "}
+        <Route path="/projects" element={<Projects />} />{" "}
+      </Routes>{" "}
+    </BrowserRouter>
+  );
+}
